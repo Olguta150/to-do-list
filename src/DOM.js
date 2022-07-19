4ad057eb18d7a1c57ff3b67c9fb320ceba209ac6
Olguta Fratila <fratilaolguta150@gmail.com>
2022-07-19 20:06:59 +0300

    Add task form

--- src/DOM.js
+++ src/DOM.js
@@ -3,13 +3,51 @@ import {Project} from './project.js';
 import {projectLocalStorage} from './storage.js';
 
 class UI {
+
+    addTaskForm() {
+        const container = document.querySelector('.container');
+        const containerFormTask = document.createElement('div');
+        const form = document.createElement('div');
+        form.innerHTML = `
+            <div>
+                <label for="title">Task:</label>
+                <input type="text" id="title" name="title">
+            </div>
+            <div>
+                <label for="description">Description:</label>
+                <input type="text" id="description" name="description">
+            </div>
+            <div>
+                <label for="date">Date:</label>
+                <input type="date" id="date" name="date">
+            </div>
+            <div>
+                <label for="priority">Priority:</label>
+                <select id="priority" name="priority">
+                    <option value="select" disabled>--Select--</option>
+                    <option value="low">Low</option>
+                    <option value="normal">Normal</option>
+                    <option value="high">High</option>
+                </select>
+            </div>
+            <div class="task-btns">
+                <button class="cancel-task task-button">Cancel</button>
+                <button class="add-task task-button">Add</button>
+            </div>
+        `;
+        form.classList.add('form-task');
+        containerFormTask.classList.add('container-form-task');
+        containerFormTask.appendChild(form);
+        container.appendChild(containerFormTask);
+    }
+
     addProjectForm() {
         const container = document.querySelector('.container');
         const containerFormProject = document.createElement('div');
         const form = document.createElement('form');
         form.innerHTML = `
             <div class="form-details">
-                <label for="name">Name</label>
+                <label for="name">Name:</label>
                 <input type="text" id="name" name="name">
             </div>
             <div class="btns">
@@ -29,14 +67,17 @@ class UI {
         message.textContent = msg;
         message.classList.add('form-message');
         form.appendChild(message);
-        setTimeout(() => form.removeChild(message), 2000);
+        setTimeout(() => form.removeChild(message), 3000);
     }
     
     removeProjectForm() {
         const containerFormProject = document.querySelector('.container-form-project');
-    
         containerFormProject.remove();
-        // containerFormProject.classList.add('none');
+    }
+
+    removeTaskForm() {
+        const containerTaskProject = document.querySelector('.container-form-task');
+        containerTaskProject.remove();
     }
 
     addNewProject(item) {
@@ -67,6 +108,14 @@ const eventsDOM = () => {
         if(e.target.matches('.cancel-project')) {
             e.preventDefault();
             ui.removeProjectForm();
+        }
+        if(e.target.matches('.add-new-task')) {
+            e.preventDefault();
+            ui.addTaskForm();
+        }
+        if(e.target.matches('.cancel-task')) {
+            e.preventDefault();
+            ui.removeTaskForm();
         }
     })
 }
